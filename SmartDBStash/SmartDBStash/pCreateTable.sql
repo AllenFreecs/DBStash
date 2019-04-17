ee3a8aad6a5506a9d575deffab062da3552e358c
Allen Benzon <adolfoallen.benzon@nowcom.com.ph>
2019-04-17 14:27:17 +0800

    Initial checkin

---
+++ SmartDBStash/SmartDBStash/pCreateTable.sql
@@ -0,0 +1,27 @@
+﻿Create PROC dbo.pCreateTable(@TableName VARCHAR(max))
+AS
+BEGIN	
+BEGIN TRY
+DECLARE @tablescript VARCHAR(max)
+
+SET @tablescript = 'CREATE TABLE [dbo].' + @TableName + '(
+CreatedDate DATETIME NOT NULL,
+CreatedBy INT NULL,
+UpdatedDate DATETIME NULL,
+UpdatedBy INT NULL,
+IsActive BIT
+	)'
+
+EXEC sp_executesql @tablescript
+
+END TRY
+BEGIN CATCH
+    PRINT 'In CATCH Block'
+    IF(@@TRANCOUNT > 0)
+        ROLLBACK TRAN;
+END CATCH
+
+
+
+END
+GO
